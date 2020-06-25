784c2938ea2554b018c377fc70a4716698905a24
EleonoraTitarenko <yaroslavnaella@gmail.com>
2020-06-25 18:31:28 +0300

    Fix QuestionGenre e2e test

--- src/components/question-genre/question-genre.e2e.test.js
+++ src/components/question-genre/question-genre.e2e.test.js
@@ -1,79 +1,72 @@
 import React from "react";
-import {configure, shallow} from "enzyme";
+import Enzyme, {shallow} from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 import QuestionGenre from "./question-genre";
 
-configure({
-  adapter: new Adapter()
+Enzyme.configure({
+  adapter: new Adapter(),
 });
 
-const mock = {
-  question: {
-    type: `genre`,
+const question = {
+  type: `genre`,
+  genre: `rock`,
+  answers: [{
+    src: `path`,
     genre: `rock`,
-    answers: [
-      {
-        src: `path`,
-        genre: `rock`,
-      },
-      {
-        src: `path`,
-        genre: `jazz`,
-      },
-      {
-        src: `path`,
-        genre: `jazz`,
-      },
-      {
-        src: `path`,
-        genre: `blues`,
-      },
-    ],
-  },
+  }, {
+    src: `path`,
+    genre: `blues`,
+  }, {
+    src: `path`,
+    genre: `jazz`,
+  }, {
+    src: `path`,
+    genre: `rock`,
+  }],
 };
 
-it(`When user answers genre question form is not sent`, () => {
-  const {question} = mock;
-  const onAnswer = jest.fn;
-  const genreQuestion = shallow(
-      <QuestionGenre
-        onAnswer={onAnswer}
-        question={question}
-      />
-  );
+describe(`QuestionGenre e2e tests`, () => {
+  it(`Form is not sent on submit`, () => {
+    const onAnswer = jest.fn();
 
-  const form = genreQuestion.find(`form`);
-  const formSendPrevention = jest.fn();
-  form.simulate(`submit`, {
-    preventDefault: formSendPrevention,
+    const genreQuestionsScreen = shallow(
+        <QuestionGenre
+          question={question}
+          onAnswer={onAnswer} />
+    );
+
+    const formElement = genreQuestionsScreen.find(`form`);
+    const formSendPrevention = jest.fn();
+
+    formElement.simulate(`submit`, {
+      preventDefault: formSendPrevention,
+    });
+
+    expect(onAnswer).toHaveBeenCalledTimes(1);
+    expect(formSendPrevention).toHaveBeenCalledTimes(1);
   });
 
-  expect(onAnswer).toHaveBeenCalledTimes(1);
-  expect(formSendPrevention).toHaveBeenCalledTimes(1);
-});
+  it(`User's answer passed to callback is consistent with "userAnswer" prop`, () => {
+    const onAnswer = jest.fn((...args) => [...args]);
+    const userAnswer = [false, true, false, false];
 
-it(`User answer passed to callback is consistent with "userAnswer" prop`, () => {
-  const {question} = mock;
-  const onAnswer = jest.fn((...args) => [...args]);
-  const userAnswer = [false, true, false, false];
+    const genreQuestion = shallow(<QuestionGenre
+      onAnswer={onAnswer}
+      question={question}
+    />);
 
-  const genreQuestion = shallow(<QuestionGenre
-    onAnswer={onAnswer}
-    question={question}
-  />);
+    const form = genreQuestion.find(`form`);
+    const inputTwo = genreQuestion.find(`input`).at(1);
 
-  const form = genreQuestion.find(`form`);
-  const inputTwo = genreQuestion.find(`input`).at(1);
+    inputTwo.simulate(`change`, {target: {checked: true}});
+    form.simulate(`submit`, {preventDefault() {}});
 
-  inputTwo.simulate(`change`, {target: {checked: true}});
-  form.simulate(`submit`, {preventDefault() {}});
+    expect(onAnswer).toHaveBeenCalledTimes(1);
 
-  expect(onAnswer).toHaveBeenCalledTimes(1);
+    expect(onAnswer.mock.calls[0][0]).toMatchObject(question);
 
-  expect(onAnswer.mock.calls[0][0]).toMatchObject(question);
-  expect(onAnswer.mock.calls[0][1]).toMatchObject(userAnswer);
-
-  expect(
-      genreQuestion.find(`input`).map((it) => it.prop(`checked`))
-  ).toEqual(userAnswer);
+    expect(
+        genreQuestion.find(`input`).map((it) => it.prop(`checked`))
+    ).toEqual(userAnswer);
+  });
 });
